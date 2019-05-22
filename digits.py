2773037c4b90f4cc387bf28d54fbe7dcbb73c0c4
kvellaidurai <46344094+kvellaidurai@users.noreply.github.com>
2019-05-22 10:03:12 +0530

    Update digits.py

--- digits.py
+++ digits.py
@@ -7,3 +7,25 @@ while(num!=0):
     num=num//10
 print(sum)
     
+d2-reverse of digits
+num=int(input())
+reverse=0
+while(num>0):
+    n=num%10
+    reverse=(reverse*10)+n
+    num=num//10
+print(reverse)
+      (OR)
+num=input()
+n=num[::-1]
+print(n)
+
+
+d3-check palindrome or not
+num=input()
+n=num[::-1]
+if(num==n):
+    print("palindrome")
+else:
+    print("not palinrome")
+
