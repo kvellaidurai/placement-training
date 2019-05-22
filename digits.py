9d1e94242aba32bb3226192b3b2e30e7a92967e5
kvellaidurai <46344094+kvellaidurai@users.noreply.github.com>
2019-05-22 10:39:30 +0530

    Update digits.py

--- digits.py
+++ digits.py
@@ -42,3 +42,11 @@ if(num==sum):
     print("Amstrong")
 else:
     print("Not Amstrong")
+    
+d5-product of dgits
+num=int(input())
+product=1
+while(num!=0):
+    product=product*(num%10)
+    num=num//10
+print(product)
